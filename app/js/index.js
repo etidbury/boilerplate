60eed7d4a6c3a653cd2cd7283918106ffcef34c5
etidbury <tidbury@live.com>
2017-07-11 17:12:53 +0100

    tweak lint and adhere to spacing at end of files

--- app/js/index.js
+++ app/js/index.js
@@ -8,6 +8,12 @@ import Routes    from './Routes';
 if ( process.env.NODE_ENV !== 'production' ) {
     // Enable React devtools
     window.React = React;
+
+    require('spacecms')(function onUpdateProjectSpaces(spaces){
+        console.info("SpaceCMS:","Updated Spaces",spaces);//fordebug: debug print
+    });
+
+
 }
 
 ReactDOM.render(Routes, document.getElementById('app'));
